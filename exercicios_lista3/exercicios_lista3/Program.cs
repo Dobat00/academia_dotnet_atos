68f27e32602a9787df6a3b3ff7cb693516f87490
Matheus Kenzo <100180936+Dobat00@users.noreply.github.com>
2022-10-04 17:54:32 -0300

    Add files via upload

--- exercicios_lista3/exercicios_lista3/Program.cs
+++ exercicios_lista3/exercicios_lista3/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace exercicios_lista3
+﻿using System.Runtime.InteropServices;
+using System.Xml;
+
+namespace exercicios_lista3
 {
     internal class Program
     {
@@ -99,7 +102,7 @@
 
             //exercicio 4
             /*
-            int joao= 0, zeca= 0, branco= 0;
+            int joao= 0, zeca= 0, branco= 0, nulo = 0;
             string resposta;
             while (true)
             {
@@ -118,22 +121,493 @@
                 {
                     branco = branco + 1;
                 }
-                else
+                else if (resposta == "fim")
                 {
                     Console.WriteLine("Votos em JOAO: " + joao +
                         "\nVotos em ZECA: " + zeca +
                         "\nVotos em BRANCO: " + branco);
+                    if (joao > zeca)
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("o vencedor foi Joao" +
+                            "\nVotos nulos: " + nulo+
+                            "\nTotal de votos: " + joao + zeca + nulo + branco);
+                    }
+                    else if (zeca > joao)
+                    {
+                        Console.WriteLine("\n\n");
+                        Console.WriteLine("o vencedor foi Zeca" +
+                            "\nVotos nulos: " + nulo +
+                            "\nTotal de votos: " + joao + zeca + nulo + branco);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nenhum dos dois candidatos venceram");
+                    }
                     break;
 
                 }
-            
-
-                
+                else
+                {
+                    nulo = nulo + 1;
+                }
             }
             */
 
 
-            //exercicio 5
+            //exercicio 5 (modificar o exercicio anterior)
+
+
+            //exercicio 6
+            /*
+            int idade;
+            while (true)
+            {
+                Console.WriteLine("Qual a sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                if (idade > 0)
+                {
+                    Console.WriteLine("A sua idade é: " + idade);
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            */
+
+
+            //exercicio 7
+            /*
+            string nome, numeros = "0123456789";
+            float salario;
+            int idade;
+            while (true)
+            {
+                Console.WriteLine("Qual o seu nome?\n");
+                nome = Console.ReadLine();
+                if (nome == " " || nome == "")
+                {
+                    Console.WriteLine("Por favor, digite um nome valido");
+                    break;
+                }
+                else
+                {
+                    for (int i = 0; i < numeros.Length; i++)
+                    {
+                        if (nome.Contains(numeros[i]))
+                        {
+                            Console.WriteLine("Nome invalido.");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Digite sua idade: ");
+                            idade = int.Parse(Console.ReadLine());
+                            if (idade <= 0)
+                            {
+                                Console.WriteLine("Digite uma idade valida");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Digite o seu salario: ");
+                                salario = float.Parse(Console.ReadLine());
+                                if (salario <= 0)
+                                {
+                                    Console.WriteLine("Valor invalido");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Salario: " + salario +
+                                        "\nNome: " + nome +
+                                        "\nIdade: " + idade);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+            */
+
+
+            //exercicio 8
+            /*
+            bool controle = true;
+            int valor;
+            string resposta;
+            while (controle)
+            {
+                Console.WriteLine("Digite um numero inteiro:\n");
+                valor = int.Parse(Console.ReadLine());
+                if (valor % 2 == 0)
+                {
+                    Console.WriteLine("É um numero par");
+                }
+                else
+                {
+                    Console.WriteLine("É um numero impar");
+                }
+                Console.WriteLine("Deseja repetir o programa? s/n");
+                resposta = Console.ReadLine();
+                resposta = resposta.ToLower();
+                if (resposta == "s")
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            */
+
+
+            //ex 9
+            /*
+            int g1=0, g2=0, g3=0, sem_filhos = 0 , filhos;
+            for (int i = 0; i <30; i++)
+            {
+                Console.WriteLine("Quantos filhos voce tem?");
+                filhos = int.Parse(Console.ReadLine());
+                if (filhos >= 1 && filhos <= 3)
+                {
+                    g1++;
+                }
+                else if(filhos >= 4 && filhos <= 7)
+                {
+                    g2++;
+                }
+                else if(filhos >=8)
+                {
+                    g3++;
+                }
+                else
+                {
+                    sem_filhos++;
+                }
+                Console.WriteLine("Pessoas entre 1 e 3 filhos: " + g1 +
+                    "\nPessoas entre 4 e 7 filhos: " + g2 +
+                    "\nPessoas com mais de 8 filhos:" + g3 +
+                    "\nPessoas sem filhos: " + sem_filhos);
+            }
+            */
+
+
+            //ex 10
+            /*
+            string nome, resposta;
+            int homem=0, mulher=0, menos_30=0, mais_60=0, media_idade_mulheres=0,idade;
+            for (int i = 0; i< 50; i++)
+            {
+                Console.WriteLine("Qual o seu nome: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("Qual o seu sexo? m/f: ");
+                resposta = Console.ReadLine();
+                resposta = resposta.ToLower();
+                if(resposta == "m")
+                {
+                    homem++;
+                }
+                else
+                {
+                    mulher++;
+                }
+                Console.WriteLine("Qual a sua idade?: ");
+                idade = int.Parse(Console.ReadLine());
+
+                if(idade <= 30)
+                {
+                    menos_30++;
+                }
+                else if(idade >= 60)
+                {
+                    mais_60++;
+                }
+            }
+            */
+
+
+            //ex11
+            /*
+            bool check = true;
+            int nota, alto=0, baixo=0, soma=0,total_alunos=0,media;
+            while (check)
+            { 
+                Console.WriteLine("Digite a nota do aluno (digite -1 para sair): ");
+                nota = int.Parse(Console.ReadLine());
+                total_alunos++; 
+                soma = soma + nota;
+                if(nota == -1)
+                {
+                    break;
+                }
+                else if (nota > alto)
+                {
+                    alto = nota;
+                }
+                else if (nota < baixo)
+                {
+                    baixo = nota;
+                }
+            }
+            media = soma / total_alunos;
+            Console.Write("A nota mais alta foi: " + alto+
+                "\nA nota mais baixa foi: " + baixo+
+                "\nA media geral foi de: "+media);
+            */
+
+
+            //ex12
+            /*
+            int soma=0;
+            for (int i = 0; i <= 100; i++)
+            {
+                soma = soma + i;
+            }
+            Console.WriteLine("A soma dos 100 primeiros numeros é de: " + soma);
+            */
+
+
+            //ex13
+            /*
+            int soma = 0;
+            for (int i = 0; i <= 500; i++)
+            {
+                if (i % 2 ==0)
+                {
+                    soma = soma + i;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine("A soma dos numeros pares, de 1 a 500 é de: " + soma);
+            */
+
+
+            //ex 14
+            /*
+            double resultado;
+            for(int i = 0; i<= 10;i++)
+            {
+                resultado = Math.Pow(2, i);
+                Console.WriteLine("A potencia de 2 elevado a " + i + " é de: " + resultado);
+            }
+            */
+
+
+            //ex 15
+            /*
+            double nota1=0, nota2=0, nota3=0, nota4=0, media=0;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Qual a nota do aluno? :");
+                Console.WriteLine(i);
+                if (i == 0)
+                {
+                    nota1 = double.Parse(Console.ReadLine());
+                }
+                else if (i == 1)
+                {
+                    nota2 = double.Parse(Console.ReadLine());
+                }
+                else if (i == 2)
+                {
+                    nota3 = double.Parse(Console.ReadLine());
+                }
+                else if (i == 3)
+                {
+                    nota4 = double.Parse(Console.ReadLine());
+                }
+
+            }
+            media = ((nota1 * 2) + (nota2 * 1) + (nota3 * 2) + (nota4 * 4) / 9);
+            Console.WriteLine("A media do aluno foi de: " + media);
+            if (media >= 7)
+            {
+                Console.WriteLine("O aluno passou!")
+            }
+            else
+            {
+                Console.WriteLine("O aluno NAO passou");
+            }
+            */
+
+
+            //ex 16
+            /*
+            int resultado = 0, valor= 0, i = 0;
+            Console.WriteLine("Digite um valor inteiro (ate 50)\n");
+            valor = int.Parse(Console.ReadLine());
+            if(valor > 50)
+            {
+                Console.WriteLine("Valor invalido!");
+            }
+            else
+            {
+                while(resultado< 250)
+                { 
+                    resultado = valor + (valor * (3 * i));
+                    if (resultado <= 250)
+                    {
+                        Console.WriteLine("O resultado esta em: " + resultado);
+                        i++;
+                    }
+                    else if (resultado >= 250)
+                    {
+                        Console.WriteLine("O resultado final é de: " + resultado);
+                        i++;
+                    }
+                    
+                    
+                }
+            }
+            */
+
+
+            //ex17
+            /*
+            int resultado;
+            for (int i = 15; i< 200; i++)
+            {
+                resultado = i * i;
+                Console.WriteLine("O quadrado de " + i + " é: " + resultado);
+            }
+            */
+
+
+            //ex18
+            /*
+            Random rnd = new Random();
+            double bas = rnd.Next(1, 1000), exp= rnd.Next(1,1000), resultado;
+            resultado = Math.Pow(bas, exp);
+            Console.WriteLine("Base: " + bas +
+                "\nExpoente: " + exp +
+                "\nPotencia: " + resultado);
+            */
+
+
+            //ex19
+            /*
+            bool check = true;
+            double salario_total = 0, salario, media_salario = 0, media_filhos=0, maior_salario = 0;
+            int filhos_total=0, filhos, populacao = 0,menos_100=0;
+            while (check)
+            {
+                Console.WriteLine("Qual o seu salario?(Digite um valor negativo para finalizar o programa): ");
+                salario = double.Parse(Console.ReadLine());
+                if (salario > 0)
+                {
+                    salario_total = salario_total + salario;
+                    populacao++;
+                    if(salario > maior_salario)
+                    {
+                        maior_salario = salario;
+                    }
+                    if(salario <= 100)
+                    {
+                        menos_100++;
+                    }
+                }
+                else if (salario < 0)
+                {
+                    break;
+                }
+         
+                Console.WriteLine("Quantos filhos voce tem?");
+                filhos = int.Parse(Console.ReadLine());
+                filhos_total = filhos + filhos_total;
+                populacao++;
+
+            }
+            media_filhos = filhos_total / populacao;
+            media_salario = salario_total / populacao;
+            menos_100 = populacao * (menos_100 / 100);
+            Console.WriteLine("Media de filhos: " + media_filhos +
+                "\nMedia salarial: " + media_salario +
+                "\nMaior salario: " + maior_salario +
+                "\n% da populacao que recebe menos que 100 reais: " + menos_100);
+            */
+
+
+            //ex20
+            
+            bool check = true;
+            string sexo,olhos, cabelo;
+            int idade, homem=0, mulher=0, loiro=0, castanho=0, preto=0, olho_azul=0, olho_verde=0, olho_castanho=0, maior_idade = 0, pessoas=0;
+
+            while (check)
+            {
+                Console.WriteLine("Qual o seu sexo?(m: masculino/f: feminino) ");
+                sexo = Console.ReadLine();
+                sexo = sexo.ToLower();
+                if (sexo == "m")
+                {
+                    homem++;
+                }
+                else
+                {
+                    mulher++;
+                }
+                Console.WriteLine("Qual a cor dos seus olhos?(a:azuis / v: verdes / c:castanhos) ");
+                olhos = Console.ReadLine();
+                olhos = olhos.ToLower();
+                if(olhos == "a")
+                {
+                    olho_azul++;
+                }
+                else if(olhos == "v")
+                {
+                    olho_verde++;
+                }
+                else
+                {
+                    olho_castanho++;
+                }
+                Console.WriteLine("Qual a cor dos seus cabelos? (l: louro/ c:castanho/ p:preto)");
+                cabelo = Console.ReadLine();
+                cabelo = cabelo.ToLower();
+                if (cabelo == "l")
+                {
+                    loiro++;
+                }
+                else if (cabelo == "c")
+                {
+                    castanho++;
+                }
+                else
+                {
+                    preto++;
+                }
+                Console.WriteLine("Qual a sua idade?(digite 0 para sair)");
+                idade = int.Parse(Console.ReadLine());
+                if (idade > 18 && idade < 35)
+                {
+                    if (olhos == "v" && cabelo == "l")
+                    {
+                        pessoas++;
+                    }
+                }
+                else if (idade == 0)
+                {
+                    break;
+                }
+                else if(idade > maior_idade)
+                {
+                    maior_idade = idade;
+                }
+                Console.WriteLine("Processando...");
+
+            }
+            Console.WriteLine("A maior idade dos habitantes: " + maior_idade +
+                "\nPessoas com cabelo loiro e olhos verdes, entre 18 e 35 anos de idade: " + pessoas);
         }
     }
 }
+                    
--- lista_de_exercicios4/lista_de_exercicios4/Program.cs
+++ lista_de_exercicios4/lista_de_exercicios4/Program.cs
@@ -147,6 +147,7 @@ namespace lista_de_exercicios4
 
 
             //ex 6
+            /*
             int[] lista = new int[10],listai = new int[10];
             int controle = 0;
 
@@ -164,6 +165,326 @@ namespace lista_de_exercicios4
             {
                 Console.WriteLine(listai[i]);
             }
+            */
+
+
+            //ex 7
+            /*
+            int[] lista = new int[10], lista2 = new int[10];
+            int controle = 0;
+            for (int i= 0; i < lista.Length; i++)
+            {
+                Console.WriteLine("Digite um valor pra lista");
+                lista[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < lista.Length - 1; i++)
+            {
+                if (lista[i] % 2 == 0)
+                {
+                    lista2[0] = lista[i];
+                }
+            }
+            */
+
+            //ex 8
+            /*
+            int[] lista = new int[30], menor_que_zero = new int[30];
+            int controle = 0;
+
+            for(int i = 0; i< lista.Length; i++)
+            {
+                Console.WriteLine("DIgite um valor pra lista");
+                lista[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            for(int i = 0; i < lista.Length; i++)
+            {
+                if (lista[i] < 0)
+                {
+                    menor_que_zero[0] = lista[i];
+                    controle++;
+                }
+                
+            }
+
+
+            for(int i = 0; i< menor_que_zero.Length; i++)
+            {
+                Console.WriteLine(menor_que_zero [i]);
+            }
+            */
+
+
+            //ex 9
+            /*
+            int[] lista = new int[10], crescente = new int[10];
+            int menor;
+
+            for (int i = 0; i< lista.Length; i++)
+            {
+                Console.WriteLine("Digite um valor pra lista: \n");
+                lista[i] = int.Parse(Console.ReadLine());
+            }
+            menor = lista[0];
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                for(int j = 0; j< lista.Length; i++)
+                {
+                    if (lista[i] < lista[j])
+                    {
+                        menor = lista[i];
+                    }
+                    else
+                    {
+                        menor = lista[j];
+                    }
+                }
+                crescente[i] = menor;
+            }
+
+            for (int i = 0; i< crescente.Length; i++)
+            {
+                Console.WriteLine(crescente[i]);
+            }
+            */
+
+
+            //ex10
+            /*
+            int[] vetor = new int[20];
+            int tamanho=0;
+            for (int i = 0; i < vetor.Length - 1; i++)
+            {
+                Console.WriteLine("Digite um valor para o vetor: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0;i< vetor.Length-1; i++)
+            {
+                if (vetor[i] == 0)
+                {
+                    tamanho++;
+                }
+            }
+            if (tamanho > 0)
+            {
+                int[] vetor2 = new int[tamanho];
+                for (int i = 0; i < vetor2.Length; i++)
+                {
+                    vetor2[i] = 2;
+                }
+                Console.WriteLine("Vetor 2:\n");
+                for (int i = 0; i < vetor2.Length; i++)
+                {
+                    Console.WriteLine(vetor2[i]);
+                }
+            }
+            Console.WriteLine("Vetor 1:\n ");
+            for (int i = 0;i< vetor.Length; i++)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+            */
+
+            //ex11
+            /*
+            int[] vetor = new int[5];
+            for (int i = 0; i <= vetor.Length - 1; i++)
+            {
+                Console.WriteLine("Digite um valor para o vetor: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Vetor invertido: ");
+            for (int i = vetor.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+            */
+
+
+            //ex12
+            /*
+            int[] vetor = new int[10];
+            int valor;
+            for (int i = 0; i <= vetor.Length -1; i++)
+            {
+                Console.WriteLine("Qual o valor que deseja colocar:");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Qual valor deseja procurar? ");
+            valor = int.Parse(Console.ReadLine());
+            for (int i = 0; i<= vetor.Length; i++)
+            {
+                if (vetor[i]== valor)
+                {
+                    Console.WriteLine(valor + " se encontra no vetor! Na posicao "+ i);
+                }
+                else
+                {
+                    Console.WriteLine("o valor nao foi encontrado");
+                }
+            }
+            */
+
+
+            //ex13
+            /*
+            int[] vetor = new int[10];
+            int dois = 0, quatro = 0, oito = 0;
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine("DIgite o valor que deseja colocar: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i] == 2)
+                {
+                    dois++;
+                }
+                else if (vetor[i] == 4)
+                {
+                    quatro++;
+                }
+                else if (vetor[i] == 8)
+                {
+                    oito++;
+                }
+            }
+            Console.WriteLine("Quantidades de 2: " + dois +
+                "Quantidades de 4: " + quatro +
+                "Quantidades de 8: " + oito);
+            */
+
+
+            //ex14
+            /*
+            int[] vetor = new int[50];
+            int resposta;
+            Console.WriteLine("Digite um codigo(0 = fim/ 1= mostrar o vetor/2= mostrar o vetor invertido: "):
+            resposta = int.Parse(Console.ReadLine());
+            if (resposta == 0)
+            {
+                break;
+            }
+            else if(resposta == 1)
+            {
+                for(int i = 0; i < vetor.Length; i++)
+                {
+                    Console.WriteLine(vetor[i]);
+                }
+            }
+            else if(resposta == 2)
+            {
+                for(int i = vetor.Length-1; i >= 0; i--)
+                {
+                    Console.WriteLine(vetor[i]);
+                }
+            }
+
+
+
+
+            
+
+            //ex de ordenacao 
+            /*
+            int[] vetor = new int[5] { 10, 7, 3, 4, 2 };
+            int aux = 0;
+            for (int i = 0; i< 5; i++)
+            {
+                for (int j=0; j<4; j++)
+                {
+                    if (vetor[j + 1] < vetor[j])
+                    {
+                        aux = vetor[j];
+                        vetor[j] = vetor[j + 1];
+                        vetor[j + 1] = aux;
+                    }
+                }
+            }
+            for (int i = 0; i< 5; i++)
+            {
+                Console.WriteLine("vetor[" + i + "]= " + vetor[i]);
+            }
+            */
+
+
+            //ex 16
+            /*
+            int[] x = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] y = new int[10] { 1, 6, 30, 4, 5, 60, 7, 9, 10, 11 };
+            int[] uniao = new int[20];
+            int[] diferenca = new int[10];
+            int[] interseccao = new int[10];
+            Console.WriteLine("UNIÃO");
+            x.CopyTo(uniao, 0);
+            int auxU = 10;
+            for (int j = 0; j < x.Length; j++)
+            {
+                if (y[i] == x[j])
+                {
+                    break;
+                }
+                if (j == (x.Length - 1))
+                {
+                    uniao[auxU] = y[i];
+                    auxU++;
+                }
+            }
+        
+            for (int i = 0; i<auxU; i++)
+            {
+                Console.WriteLine(uniao[i]);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("DIFERENÇA");
+            int auxD = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = 0; j < y.Length; j++)
+                {
+                    if (x[i] == y[j])
+                    {
+                        break;
+                    }
+                    if(j == (y.Length-1))
+                    {
+                        diferenca[auxD] = x[i];
+                        auxD++;
+                    }
+                }
+            }
+            for (int i = 0; i < auxD; i++)
+            {
+                Console.WriteLine(diferenca[i]);
+            }
+            Console.WriteLine("");
+
+
+            Console.WriteLine("INTERSECÇÃO");
+            int auxI = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = 0; j < y.Length; j++)
+                {
+                    if (x[i] == y[j])
+                    {
+                        interseccao[auxI] = x[i];
+                        auxI++;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < auxI; i++)
+            {
+                Console.WriteLine(interseccao[i]);
+            }
+            Console.WriteLine("");
+            */
 
 
         }
